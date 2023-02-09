f5c7dfd2bf5f7199ba377d4b63b98d4f30c01982
Aditya Patil <patiladityadr@gmail.com>
2023-02-09 10:37:58 +0530

    Added create function and some styling around it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
+import "./app.css";
+
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+
 function App() {
+
+    const [books, setBooks] = useState([]);
+
+
+    const createBook = (title) => {
+        const updatedBooks = [...books, {
+            id: Math.floor(Math.random() * 1000),
+            title
+        }];
+        setBooks(updatedBooks);
+
+    };
+
     return (
-        <div >
-            App
+
+        <div>
+            <BookList
+                books={books} />
+            <BookCreate onCreate={createBook} />
         </div>
     );
 }
+
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,35 @@
-function BookCreate() {
+import { useState } from 'react';
+
+
+
+
+function BookCreate({ onCreate }) {
+
+    const [title, setTitle] = useState('');
+
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    };
+
     return (
-        <div >
-            BookCreate
+        <div className='book-create'>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Title
+                </label>
+                <input
+                    type="text"
+                    value={title}
+                    onChange={handleChange} />
+                <button type="submit">Create</button>
+            </form>
         </div>
     );
 }
